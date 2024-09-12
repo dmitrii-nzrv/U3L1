f907e1a897b9a5b882cc6a5886478aad7b645594
Dmitrii <dmitrii.nazarov7703@gmail.com>
2024-09-12 18:15:34 +0300

    final

---
+++ lesson1/NetworkManager/NetworkManager.swift
@@ -0,0 +1,77 @@
+//
+//  NetworkManager.swift
+//  lesson1
+//
+//  Created by Dmitrii Nazarov on 12.09.2024.
+//
+
+//error nil
+
+import Foundation
+
+//singlot/completion/mvp/mvc
+class NetworkManager{
+    
+    func sendRequest(q: String, limit: Int){
+        
+        //1 create URL
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "https"
+        urlComponent.host = "rickandmortyapi.com"
+        urlComponent.path = "/api/character"
+    
+        urlComponent.queryItems = [
+            URLQueryItem(name: "page", value: "1"),
+        ]
+        
+        guard let url = urlComponent.url else { return }
+        
+        //2 Create Requst
+        var request = URLRequest(url: url)
+        
+        //3 send requst
+        URLSession.shared.dataTask(with: request) { data, responce, error in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+        
+            guard let data else { return }
+            
+            do {
+                let result = try JSONDecoder().decode(Responce.self, from: data)
+
+                result.results.forEach({ item in
+                    print(item.name)
+                    print(item.species)
+                    print(item.gender)
+                    print(item.origin.name)
+                    print("======")
+                    
+                })
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+        
+    }
+    
+}
+
+//Model
+struct Responce: Codable{
+    //let totalResults: Int
+    let results: [Articles]
+}
+
+struct Articles: Codable{
+    let name: String?
+    let species: String?
+    let gender: String?
+    let origin: Origin
+}
+
+struct Origin: Codable {
+    let name: String?
+}
--- lesson1/ViewController.swift
+++ lesson1/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    let networManager = NetworkManager()
+     
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         
+         networManager.sendRequest(q: "ios разработка", limit: 10)
+     }
 
 
 }
